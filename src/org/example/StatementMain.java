3a7096e70a36d5067833d9e6aed9d3306b3f6921
Pratyasha <pratyashanp192@gmail.com>
2020-09-30 18:09:40 +0530

    my 5th commit

---
+++ src/org/example/StatementMain.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class StatementMain {
+    public static void main(String[] args) {
+        String name = "Pratyasha";
+        int marks = 75;
+
+        System.out.println(name);
+        System.out.println(marks);
+    }
+}
