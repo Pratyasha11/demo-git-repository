ae5e510bafe6483025650d123d4e0b16e079b1ee
Pratyasha <pratyashanp192@gmail.com>
2020-09-30 18:23:15 +0530

    this is my 7th commit

---
+++ src/org/identifiers/IdentifierMain.java
@@ -0,0 +1,12 @@
+package org.identifiers;
+
+public class IdentifierMain {
+    public static void main(String[] args) {
+        int age = 20; // variableName: age is an identifier
+        int AGE = 30;
+        int Age = 40;
+        System.out.println(age);
+        System.out.println(AGE);
+        System.out.println(Age);
+    }
+}
