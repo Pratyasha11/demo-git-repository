481a821319a79842ae02c07880b4eb37a610812f
Pratyasha <pratyashanp192@gmail.com>
2020-09-30 18:16:22 +0530

    this is my 6th commit

--- src/org/example/StatementMain.java
+++ src/org/example/StatementMain.java
@@ -7,5 +7,21 @@ public class StatementMain {
 
         System.out.println(name);
         System.out.println(marks);
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+        System.out.println(sum);
+
+
+        int age = 17;
+        if( age >= 18)
+        {
+            System.out.println("you are elligible to vote in the election");
+        }
+        else{
+            System.out.println("you are not elligible to vote in the election!!!");
+        }
     }
-}
+    }
