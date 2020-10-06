eef7db318a990efc493871f18283c95d63b573e1
Pratyasha <pratyashanp192@gmail.com>
2020-10-06 21:33:02 +0530

    code for user defined constructor

---
+++ src/org/oops/constructors/UserDefinedConstructorMain.java
@@ -0,0 +1,9 @@
+package org.oops.constructors;
+
+public class UserDefinedConstructorMain {
+    public static void main(String[] args) {
+
+        Item item1 = new Item(1,"tea bag", 100);
+
+    }
+}
