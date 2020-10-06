a71020d9dc24e4de2fc44d0d904727cd49c53f0a
Pratyasha <pratyashanp192@gmail.com>
2020-10-06 21:41:49 +0530

    another one using this keyword

---
+++ src/org/oops/thiskeyword/ProductMain.java
@@ -0,0 +1,13 @@
+package org.oops.thiskeyword;
+
+public class ProductMain {
+
+    public static void main(String[] args) {
+
+        Product p = new Product(1, "tea bag", 100);
+
+
+        System.out.println(p.hashCode());
+
+    }
+}
